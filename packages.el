38ccd329ad82a7e25fbce6a7d9858b5ae3296280
Bryan McCoid <bryan@shieldx.com>
2020-03-08 15:29:20 -0700

    initial commit for tabs-layer
    Uses centaur-tabs to create a vim-ish tabs. It is largely un-customized
    currently but this seemed to be the only way to actually get it to load on
    spacemacs boot.

---
+++ packages.el
@@ -0,0 +1,73 @@
+;;; packages.el --- tabs layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: bryan mccoid <bryan@bryan-dev-machine>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `tabs-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `tabs/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `tabs/pre-init-PACKAGE' and/or
+;;   `tabs/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst tabs-packages
+  '(centaur-tabs)
+  "The list of Lisp packages required by the tabs layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+(setq tabs-packages '(centaur-tabs))
+(defun tabs/init-centaur-tabs ()
+  (use-package centaur-tabs)
+
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-height 30)
+  (setq centaur-tabs-set-bar 'under)
+  (setq x-underline-at-descent-line t)
+
+  (centaur-tabs-mode 1))
+
+;;; packages.el ends here
